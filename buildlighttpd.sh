deb2b26060e421f6e2e84393c9699d02adf001cc
leo <leonardo@cantarella.it>
2024-08-14 19:16:24 +0200

    current stage: tried running fuzzer with wait and no wait for connection, the last do low coverage, the first do 0 coverage. tried with asan options and without. no major changes. the only to make a lot more coverage is the one with the address sanitizer error

--- buildlighttpd.sh
+++ buildlighttpd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-./autogen.sh 
-CC=gcc CXX=g++
-CFLAGS='-g -O0' CXXFLAGS='-g -O0'
+export CC=gcc
+export CXX=g++
+./autogen.sh
+export CFLAGS='-g -O0'
+export CXXFLAGS='-g -O0'
+export ASAN_OPTIONS='abort_on_error=1:symbolize=0:detect_leaks=0:detect_stack_use_after_return=1:detect_container_overflow=0:poison_array_cookie=0:malloc_fill_byte=0:max_malloc_fill_size=16777216'
 ./configure --enable-static --disable-shared
 AFL_USE_ASAN=1 make clean all
